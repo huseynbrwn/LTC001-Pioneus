699465e06ed35d163c2acf53383c7969b93bfbd4
Huseyn <huseynhuseynovlw@gmail.com>
2023-12-19 12:24:33 +0400

    Commit1

--- src/Lesson5/Bolme.java
+++
@@ -1,7 +0,0 @@
-package Lesson5;
-
-public class Bolme {
-    public static void main(String[] args) {
-        System.out.println();
-    }
-}
---
+++ src/Today/Actor.java
@@ -0,0 +1,15 @@
+package Today;
+
+class Actor {
+    String name;
+    int age;
+    int actingSkill;
+    String gender;
+
+    public Actor(String name, int age, int actingSkill, String gender) {
+        this.name = name;
+        this.age = age;
+        this.actingSkill = actingSkill;
+        this.gender = gender;
+    }
+}
