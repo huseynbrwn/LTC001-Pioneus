7a6b59aabe1207405298ade4cf612cec823c1339
Huseyn <huseynhuseynovlw@gmail.com>
2024-03-07 16:37:45 +0400

    Commit1

--- src/Lesson2/AgeClassifier.java
+++ src/Lesson2/AgeClassifier.java
@@ -5,14 +5,25 @@ import java.util.Scanner;
 public class AgeClassifier {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("How old are you? ");
         int age = scanner.nextInt();
 
-        if (age < 18) {
-            System.out.println("You are young.");
-        } else if (age <= 35) {
-            System.out.println("You are middle-aged.");
+        if (age > 0) {
+            System.out.println(name + " is " + age);
+
+            if (age < 18) {
+                System.out.println(name + " is still a child");
+            } else if (age < 35) {
+                System.out.println(name + " is young");
+            } else {
+                System.out.println(name + " is adult");
+            }
         } else {
-            System.out.println("You are old.");
+            System.out.println("Age is entered incorrectly");
         }
     }
 }
